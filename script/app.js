1bd32bc8fe616f91043ce676fc3566441c33129e
Soma <Soma@example.com>
2021-01-25 03:00:47 +0500

    First JS file

--- script/app.js
+++ script/app.js
@@ -1,5 +1,17 @@
-let num;
+const
+	numberofFilms = +prompt("Сколько фильмов вы уже посмотрели?", ""),
+	personalMovieBD = {
+		count: numberofFilms,
+		movies: {},
+		actors: {},
+		genres: [],
+		privat: false,
+	};
 
-if (true || (num = 7)) {
-	console.log(num);
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Один из последних просмотренных фильмов?", "");
+	let b = prompt("На сколько оцените его?", "");
+	personalMovieBD.movies.a = b;
 }
+
+console.log(personalMovieBD);
